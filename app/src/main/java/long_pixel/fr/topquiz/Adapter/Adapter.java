53df9b7146898e90cd4774445002ea3809a36b94
Romain <rmathieu13@gmail.com>
2017-11-26 01:16:29 +0100

    add arrayList pseudo and score + write var sharedPreferences - adpter.java
    Signed-off-by: Romain <rmathieu13@gmail.com>

--- app/src/main/java/long_pixel/fr/topquiz/Adapter/Adapter.java
+++ app/src/main/java/long_pixel/fr/topquiz/Adapter/Adapter.java
@@ -1,11 +1,13 @@
 package long_pixel.fr.topquiz.Adapter;
 
+import android.content.SharedPreferences;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import long_pixel.fr.topquiz.R;
 
@@ -15,11 +17,14 @@ import long_pixel.fr.topquiz.R;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
-    ArrayList<String> listePseudo = new ArrayList<>();
-    ArrayList<Integer> listScore = new ArrayList<>();
+    private List<String> listePseudo;
+    private ArrayList<Integer> listScore;
+    private SharedPreferences mPreferences;
+    private int score;
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.row_listview, parent, false);
         return new ViewHolder(view);
